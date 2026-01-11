9097e93b7b88e33485f51eed63cbbf82511de1b7
ANKIT GUPTA <ankit12.22gupta@gmail.com>
2026-01-11 12:58:10 +0530

    .

---
+++ 11-01-2026/16.cpp
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/3sum-closest/description/
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int closestSum = nums[0] + nums[1] + nums[2];
+        int minDiff = abs(closestSum - target);
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                int diff = abs(sum - target);
+                if (diff < minDiff) {
+                    minDiff = diff;
+                    closestSum = sum;
+                }
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return target;
+                }
+            }
+        }
+        return closestSum;
+    }
+};
