7418023a562362acabfff65f58dec4e665129313
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-06-23 08:14:21 +0530

    Time: 0 ms (100%), Space: 10 MB (5.8%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int mx = *max_element(nums.begin(), nums.end());
-
-        vector<int> count(mx+1, 0);
-        for(auto& i:nums){
-            count[i]++;
-        }
+        vector<int> freq(101, 0);
         int ans = 0;
-        for (int freq : count) {
-            if (freq >= 2) {
-                ans += freq * (freq - 1) / 2;
-            }
+
+        for (int num : nums) {
+            ans += freq[num];
+            freq[num]++;
         }
+
         return ans;
     }
 };
