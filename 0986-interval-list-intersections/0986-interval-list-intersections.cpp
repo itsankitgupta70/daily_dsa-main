03af4afc6afdbafd0d5807d8322d038337f85998
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-07-02 15:30:56 +0530

    Time: 0 ms (100%), Space: 22.9 MB (84.94%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,21 +5,13 @@ public:
         vector<vector<int>> ans;
         int i = 0, j = 0;
         while (i < firstList.size() && j < secondList.size()) {
-            int start1 = firstList[i][0];
-            int end1 = firstList[i][1];
-            int start2 = secondList[j][0];
-            int end2 = secondList[j][1];
+            int start = max(firstList[i][0], secondList[j][0]);
+            int end = min(firstList[i][1], secondList[j][1]);
 
-            if (start1 <= start2) {
-                if (end1 >= start2) {
-                    ans.push_back({max(start1, start2), min(end1, end2)});
-                }
-            } else {
-                if (end2 >= start1) {
-                    ans.push_back({max(start1, start2), min(end1, end2)});
-                }
+            if (start <= end) {
+                ans.push_back({start, end});
             }
-            if (end1 <= end2) {
+            if (firstList[i][1] <= secondList[j][1]) {
                 i++;
             } else {
                 j++;
