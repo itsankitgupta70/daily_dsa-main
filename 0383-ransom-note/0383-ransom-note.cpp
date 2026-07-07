62e58948a96e9061fa90112f3ab5dfe7082e2064
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-07-07 15:29:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (89.43%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> need(26, 0);
-        for (auto& ransom : ransomNote) {
-            need[ransom - 'a']++;
-        }
-        vector<int> have(26, 0);
-        for (auto& mag : magazine) {
-            have[mag - 'a']++;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (have[i] < need[i]) {
+        if (ransomNote.size() > magazine.size())
+            return false;
+        int freq[26] = {0};
+
+        for (char ch : magazine)
+            freq[ch - 'a']++;
+
+        for (char ch : ransomNote) {
+            if (--freq[ch - 'a'] < 0)
                 return false;
-            }
         }
+
         return true;
     }
 };
