8d7c9d5633569fa34e405501f95e7e527a8e2056
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-07-09 09:26:30 +0530

    Time: 53 ms (5.66%), Space: 53.3 MB (65.5%) - LeetHub

--- 2667-create-hello-world-function/2667-create-hello-world-function.js
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -1,14 +1,5 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World"
-    }
-};
-
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+function createHelloWorld(){
+    return function(...args){
+        return "Hello World";
+    };
+}
