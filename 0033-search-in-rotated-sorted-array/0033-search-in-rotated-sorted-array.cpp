84b33be2a4a8b99d134a70b2630edf3d132ba577
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-08-11 18:09:08 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.89%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,42 +2,27 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-// beech ka point
-        int low = 0, high = n - 1;
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] > nums[high])
-                low = mid + 1;
-            else
-                high = mid;
-        }
-
-        int pivot = low;
-
-// target kaha pe hai left ya right me
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            low = pivot;
-            high = n - 1;
-        }
-        else {
-            low = 0;
-            high = pivot - 1;
-        }
-
+        int low = 0;
+        int high = n - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target)
+            if (nums[mid] == target) {
                 return mid;
-
-            if (nums[mid] < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+            }
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
         }
-
         return -1;
     }
 };
