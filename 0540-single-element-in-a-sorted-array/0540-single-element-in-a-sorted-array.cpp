53276107a57f1e7d00e161de40db801dc5496510
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-08-12 11:35:29 +0530

    Time: 0 ms (100%), Space: 26.2 MB (69.66%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
         int l = 0;
-        int h = n - 1;
+        int h = nums.size() - 1;
+
         while (l < h) {
             int mid = l + (h - l) / 2;
-            // right is even or odd??
-            bool isEven;
-            if ((h - mid) % 2 == 0) {
-                isEven = true;
-            } else {
-                isEven = false;
-            }
-            // check bagal wala mid+1 ko...
+
+            // Make mid even
+            if (mid % 2 == 1)
+                mid--;
+
             if (nums[mid] == nums[mid + 1]) {
-                if (isEven) {
-                    l = mid + 2;
-                } else {
-                    h = mid - 1;
-                }
-            } else { // nums[mid]!=nums[mid+1]
-                if (isEven) {
-                    h = mid;
-                } else {
-                    l = mid + 1;
-                }
+                // Pair is intact, single is on the right
+                l = mid + 2;
+            } else {
+                // Pair is broken, single is on the left
+                h = mid;
             }
         }
-        return nums[h];
+
+        return nums[l];
     }
 };
