ac9529f9446b646a61c68914ec7b55cff10efc33
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-08-20 10:21:22 +0530

    Time: 6 ms (97.19%), Space: 7.7 MB (82.15%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int findKthNumber(int m, int n, int k) {
-
         if (m > n)
             swap(m, n);
-
         int low = 1;
         int high = m * n;
-
         while (low <= high) {
             int mid = low + (high - low) / 2;
             int count = 0;
-
             for (int i = 1; i <= m; i++) {
                 count += min(n, mid / i);
             }
-
             if (count < k) {
                 low = mid + 1;
             }
@@ -23,7 +18,6 @@ public:
                 high = mid - 1;
             }
         }
-
         return low;
     }
 };
