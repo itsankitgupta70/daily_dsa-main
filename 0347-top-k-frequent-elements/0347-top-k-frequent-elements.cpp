8e4834a982b7f8cc369be24642943acab56124d2
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-08-24 16:21:19 +0530

    Time: 0 ms (100%), Space: 18 MB (46.99%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,29 +1,38 @@
 class Solution {
 public:
     typedef pair<int, int> P;
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // sare elements ko map kar do unke frequncy ke hisab se
+
         unordered_map<int, int> mp;
+
+        // Count frequencies
         for (auto& num : nums) {
             mp[num]++;
         }
-        // ab ek min heap banao jise frequncy ke basis pe sort karo
+
+        // Min heap based on frequency
         priority_queue<P, vector<P>, greater<P>> pq;
-        // itrate full map
+
+        // Keep only k elements
         for (auto& it : mp) {
             int val = it.first;
             int freq = it.second;
+
             pq.push({freq, val});
+
             if (pq.size() > k) {
                 pq.pop();
             }
         }
-        // result vector bana lo
+
         vector<int> res;
+
         while (!pq.empty()) {
             res.push_back(pq.top().second);
             pq.pop();
         }
+
         return res;
     }
 };
