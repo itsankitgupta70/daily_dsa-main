395ba8753ac92de86a30372006ea914c8f55b1c0
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-08-25 11:41:46 +0530

    Time: 0 ms (100%), Space: 28.6 MB (8.16%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int num : nums)
+            pq.push(num);
+
+        vector<int> ans;
+
+        while (!pq.empty()) {
+            int alice = pq.top();
+            pq.pop();
+
+            int bob = pq.top();
+            pq.pop();
+
+            ans.push_back(bob);
+            ans.push_back(alice);
+        }
+
+        return ans;
+    }
+};
