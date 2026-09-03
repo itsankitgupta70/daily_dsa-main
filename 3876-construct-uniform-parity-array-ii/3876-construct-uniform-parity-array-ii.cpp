bfc2035a7c3a05066f291f4fd7e91885334d133d
ANKIT GUPTA <139208295+itsankitgupta70@users.noreply.github.com>
2026-09-03 10:33:02 +0530

    Time: 3 ms (72.51%), Space: 165.8 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int oddMin = INT_MAX;
-        for (int i = 0; i < nums1.size(); i++) {
-            if (nums1[i] % 2 != 0) {
-                if (nums1[i] < oddMin) {
-                    oddMin = nums1[i];
-                }
-            }
+
+        for (int num : nums1) {
+            if (num % 2 != 0)
+                oddMin = min(oddMin, num);
         }
+
         if (oddMin == INT_MAX)
             return true;
-        for (int i = 0; i < nums1.size(); i++) {
-            if (nums1[i] % 2 == 0 && nums1[i] - oddMin < 1) {
+
+        for (int num : nums1) {
+            if (num % 2 == 0 && num < oddMin)
                 return false;
-            }
         }
+
         return true;
     }
 };
